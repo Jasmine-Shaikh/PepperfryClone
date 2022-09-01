95979e1059cd761d39f0aaaa21ad90d10c1dbc9c
shiwam-C114 <zaqqst@gmail.com>
2022-09-01 14:22:18 +0530

    UPATED: added dbConnect

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@
 require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
+const dbConnect = require("./dbConnect");
+const userRouter = require("./routes/userRoutes");
 const app = express()
 const port = process.env.PORT || 8080
 app.use(express.json())
 app.use(cors())
 
+dbConnect()
 
-app.listen(port, ()=>{console.log(`app running on http://localhost:${port}`); })
+// app.use(userRouter)
+
+app.listen(port, () => { console.log(`app running on http://localhost:${port}`); })
---
+++ backend/models/user.js
@@ -0,0 +1,8 @@
+// const mongoose = require('mongoose');
+
+// const UserSchema = new mongoose.Schema({
+//     username: { type: String, required: true, unique: true },
+//     password: { type: String, requred: true }
+// })
+
+// module.exports =  mongoose.model('User', UserSchema) 
