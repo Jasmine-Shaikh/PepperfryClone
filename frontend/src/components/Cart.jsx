5045c11496610399b6552ca2dcf5cca0b1339e1d
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-09-04 17:41:22 +0530

    update css

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import './cart.css'
-import { useState } from 'react';
+import React from "react";
+import "./cart.css";
+import { useState } from "react";
 
-
-import location from '../images/w21-locate-orange-icon.svg';
-import image from '../images/monster-ultimate--t--gaming-chair-in-black.webp';
-
+import logo from "../images/pf-logo-21.svg";
+import visa from "../images/web21-footer-visacard-logo.png";
+import master from "../images/web21-footer-mastercard-logo.png";
+import maestro from "../images/web21-footer-maestrocard-logo.png";
+import americanE from "../images/web21-footer-amexcard-logo.png";
+import rupay from "../images/web21-footer-rupaycard-logo.png";
+import dinnerclub from "../images/web21-footer-dinnerscard-logo.png";
+import wallet from "../images/web21-footer-wallet-logo.png";
+import internet from "../images/web21-footer-internet-banking-logo.png";
+import delievery from "../images/icon-ck-footer-hassle-free-delievry-21.svg";
+import returns from "../images/icon-ck-footer-easy-returns-21.svg";
+import trophy from "../images/icon-ck-footer-trophy-21.svg";
+import versign from "../images/icon-ck-footer-verisign-21-2x.webp";
+import psidss from "../images/icon-ck-footer-pci-21-2x.png";
+import location from "../images/w21-locate-orange-icon.svg";
+import image from "../images/monster-ultimate--t--gaming-chair-in-black.webp";
 
 const Cart = () => {
   const [cartitem, setcartitem] = useState([
     {
       heading:
-        'Monster Ultimate (T) Gaming Chair in Black & Grey Colour by Green Soul',
-      by: 'Green Soul',
-      warrenty: '36 Months Warranty, 100% Genuine',
+        "Monster Ultimate (T) Gaming Chair in Black & Grey Colour by Green Soul",
+      by: "Green Soul",
+      warrenty: "36 Months Warranty, 100% Genuine",
       price: 35333,
       delivery: 0,
       assembly: 0,
@@ -24,9 +36,9 @@ const Cart = () => {
     },
     {
       heading:
-        'Zeeyan Leatherette High Back Executive Chair In Black Color By Valuewud',
-      by: 'ValueWud',
-      warrenty: '12 Months Warranty, 100% Genuine',
+        "Zeeyan Leatherette High Back Executive Chair In Black Color By Valuewud",
+      by: "ValueWud",
+      warrenty: "12 Months Warranty, 100% Genuine",
       price: 12499,
       discount: 1299,
       delivery: 240,
@@ -38,24 +50,22 @@ const Cart = () => {
   ]);
   // const [total, settotal] = useState(0)
   console.log(cartitem[0]);
-let dollarIndianLocale = Intl.NumberFormat('en-IN');
+  let dollarIndianLocale = Intl.NumberFormat("en-IN");
 
-  var cartvalue = 0
-  var delieverya =0
-  var assembly =0
-  var discount =0
-  var credits =0
-  var total = 0
+  var cartvalue = 0;
+  var delieverya = 0;
+  var assembly = 0;
+  var discount = 0;
+  var credits = 0;
+  var total = 0;
 
- for (let i = 0; i < cartitem.length; i++) {
-  
-   cartvalue = cartvalue + cartitem[i]['price'];
-   delieverya = delieverya + cartitem[i]['delivery'];
-   assembly = assembly + cartitem[i]['assembly'];
-   discount = discount + cartitem[i]['discount'];
-   credits = credits + cartitem[i]['credit'];
-   total =  cartvalue + delieverya + assembly - discount - credits
-  
+  for (let i = 0; i < cartitem.length; i++) {
+    cartvalue = cartvalue + cartitem[i]["price"];
+    delieverya = delieverya + cartitem[i]["delivery"];
+    assembly = assembly + cartitem[i]["assembly"];
+    discount = discount + cartitem[i]["discount"];
+    credits = credits + cartitem[i]["credit"];
+    total = cartvalue + delieverya + assembly - discount - credits;
   }
   console.log(cartvalue);
   console.log(delieverya);
@@ -78,12 +88,12 @@ let dollarIndianLocale = Intl.NumberFormat('en-IN');
               <div className="saved-message">
                 <p className="jspa">
                   Congratulations! You Saved&nbsp;
-                  {(discount + credits).toLocaleString('en-IN')}
+                  {(discount + credits).toLocaleString("en-IN")}
                   &nbsp;On This Order
                 </p>
               </div>
               <div className="cart-list">
-                {cartitem?.map(item => (
+                {cartitem?.map((item) => (
                   <div className="card" key={item.id}>
                     <img className="item-image" src={item.image} alt="" />
                     <div className="cart-right">
@@ -96,7 +106,7 @@ let dollarIndianLocale = Intl.NumberFormat('en-IN');
                         ₹ {item.price - (item.discount + item.credit)}
                         &nbsp;&nbsp;&nbsp;
                         <p className="line-through">
-                          ₹ {item.price.toLocaleString('en-IN')}
+                          ₹ {item.price.toLocaleString("en-IN")}
                         </p>
                         &nbsp; &nbsp; &nbsp;
                         <p className="jspb1">
@@ -104,7 +114,7 @@ let dollarIndianLocale = Intl.NumberFormat('en-IN');
                           {(
                             item.price -
                             (item.price - (item.discount + item.credit))
-                          ).toLocaleString('en-IN')}
+                          ).toLocaleString("en-IN")}
                           )
                         </p>
                       </div>
@@ -122,21 +132,21 @@ let dollarIndianLocale = Intl.NumberFormat('en-IN');
                   <p>Remove</p>
                 </div>
                 <div className="locate">
-                  <img src={location} alt="" />
+                  {/* <img src={location} alt="" /> */}
                   <p>Locate me</p>
                 </div>
               </div>
               <div className="rtop2">
-                <div className="rcoupon">
+                {/* <div className="rcoupon">
                   <p>Apply Coupon</p>
                   <p></p>
-                </div>
+                </div> */}
               </div>
               <div className="rmid1">
                 <div className="price-detail jsc2">
                   <p>Price Details</p>
                   <p>
-                    {cartitem.length} {cartitem.length > 1 ? 'items' : 'item'}
+                    {cartitem.length} {cartitem.length > 1 ? "items" : "item"}
                   </p>
                 </div>
                 <div className="pricing">
@@ -156,11 +166,11 @@ let dollarIndianLocale = Intl.NumberFormat('en-IN');
                 <div className="discount">
                   <div className="retail jsc2">
                     <p>Retail Discount</p>
-                    <p>(-)₹ {discount.toLocaleString('en-IN')}</p>
+                    <p>(-)₹ {discount.toLocaleString("en-IN")}</p>
                   </div>
                   <div className="credits jsc2">
                     <p>Credits Reedemed</p>
-                    <p>(-)₹ {credits.toLocaleString('en-IN')}</p>
+                    <p>(-)₹ {credits.toLocaleString("en-IN")}</p>
                   </div>
                 </div>
                 <div className="total">
@@ -169,7 +179,7 @@ let dollarIndianLocale = Intl.NumberFormat('en-IN');
                       <p>Sub Total</p>
                       <p className="p11">(incl. of all taxes)</p>
                     </div>
-                    <p>₹ {total.toLocaleString('en-IN')}</p>
+                    <p>₹ {total.toLocaleString("en-IN")}</p>
                   </div>
                 </div>
                 <div className="rinfo">
@@ -186,8 +196,9 @@ let dollarIndianLocale = Intl.NumberFormat('en-IN');
           </div>
         </div>
       </div>
+      
     </div>
   );
-}
+};
 
-export default Cart
+export default Cart;
